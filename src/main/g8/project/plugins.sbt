680b72dc64eaaab658e80cbba8bd7f498548df9f
Yuvi Masory <ymasory@gmail.com>
2012-10-08 21:59:58 -0400

    first commit

---
+++ src/main/g8/project/plugins.sbt
@@ -0,0 +1,4 @@
+libraryDependencies ++= Seq(
+  "com.puppycrawl.tools" % "checkstyle" % "5.5",
+  "net.sourceforge.pmd" % "pmd" % "5.0.0"
+)
---
+++ src/main/g8/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("trailing whitespace annoys CheckStyle");     
+        if(false) {
+            System.out.println("dead code annoys PMD");
+        }
+    }
+}
